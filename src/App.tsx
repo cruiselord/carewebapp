0475b3a76caf13bf186d9f8b370b12105f8f88f2
cruiselord <omolayo007@gmail.com>
2025-02-24 19:58:42 +0100

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { useState } from "react";
 import Layout from "./components/Layout";
 import Index from "./pages/Index";
@@ -44,6 +45,7 @@ const App = () => {
             </Routes>
           </Layout>
         </BrowserRouter>
+        <Analytics />
       </TooltipProvider>
     </QueryClientProvider>
   );
